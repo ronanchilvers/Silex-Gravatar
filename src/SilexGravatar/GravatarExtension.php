232651607df73e81451531a1ed1cb4f366f1261d
Ronan Chilvers <ronan@thelittledot.com>
2017-05-04 09:03:27 +0100

    Update service provider to support Silex2

--- src/SilexGravatar/GravatarExtension.php
+++ src/SilexGravatar/GravatarExtension.php
@@ -4,23 +4,18 @@
 namespace SilexGravatar;
 
 use Silex\Application;
-use Silex\ServiceProviderInterface;
+use Silex\Api\BootableProviderInterface;
 
 use Gravatar\Service,
     Gravatar\Cache\FilesystemCache,
     Gravatar\Cache\ExpiringCache,
     Gravatar\Extension\Twig\GravatarExtension as TwigGravatarExtension;
 
-class GravatarExtension implements ServiceProviderInterface
+class GravatarExtension implements BootableProviderInterface
 {
-    public function register(Application $app)
-    {
-
-    }
-
     public function boot(Application $app)
     {
-        $app['gravatar.cache'] = $app->share(function () use ($app) {
+        $app['gravatar.cache'] = function () use ($app) {
             $cache = null;
             if(isset($app['gravatar.cache_dir'])) {
                 $ttl   = isset($app['gravatar.cache_ttl']) ? $app['gravatar.cache_ttl'] : 360;
@@ -28,19 +23,19 @@ class GravatarExtension implements ServiceProviderInterface
                 $cache = new ExpiringCache($file, $ttl);
             }
             return $cache;
-        });
+        };
 
-        $app['gravatar'] = $app->share(function () use ($app) {
+        $app['gravatar'] = function () use ($app) {
             $options = isset($app['gravatar.options']) ? $app['gravatar.options'] : array();
             return new Service($options, $app['gravatar.cache']);
-        });
+        };
 
         if (isset($app['twig'])) {
-            $app['twig'] = $app->share($app->extend('twig', function ($twig, $app) {
+            $app['twig'] = $app->extend('twig', function ($twig, $app) {
                 $twig->addExtension(new TwigGravatarExtension($app['gravatar']));
 
                 return $twig;
-            }));
+            });
         }
     }
 }
